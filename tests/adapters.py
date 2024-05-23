4759f909f0fabd9383adb9f8721434d990e6f575
manhtdd <manh.td120901@gmail.com>
2024-05-23 11:34:16 +0700

    Pass checkpointing

--- tests/adapters.py
+++ tests/adapters.py
@@ -20,7 +20,7 @@ from cs336_basics.training_utils.loss import cross_entropy_loss
 from cs336_basics.training_utils.adamw import AdamW
 from cs336_basics.training_utils.lr_schedule import cosine_with_warmup_lr_schedule
 from cs336_basics.training_utils.gradient_clipping import gradient_clipping
-from cs336_basics.training import load_data
+from cs336_basics.training import load_data, save_checkpoint, load_checkpoint
 
 def run_positionwise_feedforward(
     d_model: int,
@@ -516,7 +516,7 @@ def run_save_checkpoint(
         out: str | os.PathLike | BinaryIO | IO[bytes]
             Path or file-like object to serialize the model, optimizer, and iteration to.
     """
-    raise NotImplementedError
+    return save_checkpoint(model, optimizer, iteration, out)
 
 
 def run_load_checkpoint(
@@ -540,7 +540,7 @@ def run_load_checkpoint(
     Returns:
         int, the previously-serialized number of iterations.
     """
-    raise NotImplementedError
+    return load_checkpoint(src, model, optimizer)
 
 
 def get_tokenizer(
